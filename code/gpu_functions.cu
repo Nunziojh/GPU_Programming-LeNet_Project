442c15564239d49dc40a43f6bbbaa6177115b756
P-ietro <p.mazza00@gmail.com>
2024-04-17 16:12:06 +0200

    aggionramento file di funzioni

--- code/gpu_functions.cu
+++ code/gpu_functions.cu
@@ -77,13 +77,11 @@ __global__ void avg_pooling(float *in, float *out, int h, int w, int new_h, int 
         int new_idx = idx * stride;
         int new_idy = idy * stride;
 
-        for(i = 0; i < POOLING_WINDOW_SIZE; i++){
-            for(j = 0; j < POOLING_WINDOW_SIZE; j++){
-                tmp += ((new_idy + i) >= h || (new_idx + j) >= w) ? 0 : in[(new_idy + i) * w + new_idx + j];
+        for(i = new_idy; i < (new_idy + POOLING_WINDOW_SIZE); i++){
+            for(j = new_idx; j < (new_idx + POOLING_WINDOW_SIZE); j++){
+                tmp += (i >= h || j >= w) ? 0 : in[i * w + j];
             }
         }
-
-        __syncthreads();
 
         out[idy * new_w + idx] = tmp / (float)(POOLING_WINDOW_SIZE * POOLING_WINDOW_SIZE);
     }
@@ -102,23 +100,26 @@ __global__ void inverse_avg_pooling(float *in, float *out, float *m, int w_in, i
         int new_idx = idx * stride;
         int new_idy = idy * stride;
 
-        for(i = 0; i < POOLING_WINDOW_SIZE; i++){
-            for(j = 0; j < POOLING_WINDOW_SIZE; j++){
-                tot += ((new_idy + i) >= new_h || (new_idx + j) >= new_w) ? 0.0 : m[(new_idy + i) * new_w + new_idx + j];
+        float tmp[4];
+        for(i = 0; i < POOLING_WINDOW_SIZE * POOLING_WINDOW_SIZE; i++) tmp[i] = m[i / POOLING_WINDOW_SIZE * new_w + i % POOLING_WINDOW_SIZE];
+
+        for(i = new_idy; i < (new_idy + POOLING_WINDOW_SIZE); i++){
+            for(j = new_idx; j < (new_idx + POOLING_WINDOW_SIZE); j++){
+                tot += (i >= new_h || j >= new_w) ? 0 : tmp[i * POOLING_WINDOW_SIZE + j]; //[i * new_w + j];
             }
         }
 
         if(tot == 0.0){
-            for(i = 0; i < POOLING_WINDOW_SIZE; i++){
-                for(j = 0; j < POOLING_WINDOW_SIZE; j++){
-                    out[(new_idy + i) * new_w + new_idx + j] = 0.0;
+            for(i = new_idy; i < (new_idy + POOLING_WINDOW_SIZE); i++){
+                for(j = new_idx; j < (new_idx + POOLING_WINDOW_SIZE); j++){
+                    out[i * new_w + j] = 0.0;
                 }
             }
         }
         else {
-            for(i = 0; i < POOLING_WINDOW_SIZE; i++){
-                for(j = 0; j < POOLING_WINDOW_SIZE; j++){
-                    out[(new_idy + i) * new_w + new_idx + j] = m[(new_idy + i) * new_w + new_idx + j] / tot * in[idy * w_in + idx];
+            for(i = new_idy; i < (new_idy + POOLING_WINDOW_SIZE); i++){
+                for(j = new_idx; j < (new_idx + POOLING_WINDOW_SIZE); j++){
+                    out[i * new_w + j] = /*m[i * new_w + j]*/tmp[i * POOLING_WINDOW_SIZE + j] / tot * in[idy * w_in + idx];
                 }
             }
         }
@@ -126,7 +127,40 @@ __global__ void inverse_avg_pooling(float *in, float *out, float *m, int w_in, i
     }
 }
 
-__global__ void matrix_product(float *in1, float *in2, float *out, int w_out, int h_out, int w_in1){
+#define TILE_DIM 32
+__global__ void matrix_product(float *in1, float *in2, float *out, int w_out, int h_out, int w_in1, int piastrella) { //w_in1 rappresenta la dimensione comune delle due matrici di ingresso
+    __shared__ float sA[TILE_DIM][TILE_DIM];   // Tile size of 32x32
+    __shared__ float sB[TILE_DIM][TILE_DIM];
+
+    int Row = blockDim.y * blockIdx.y + threadIdx.y;
+    int Col = blockDim.x * blockIdx.x + threadIdx.x;
+    float Cvalue = 0.0;
+    sA[threadIdx.y][threadIdx.x] = 0.0;
+    sB[threadIdx.y][threadIdx.x] = 0.0;
+
+    for (int ph = 0; ph < (((w_in1 - 1) / piastrella) + 1); ph++) {
+        if ((Row < h_out) && (threadIdx.x + (ph * piastrella)) < w_in1) {
+            sA[threadIdx.y][threadIdx.x] = in1[(Row * w_in1) + threadIdx.x + (ph * piastrella)];
+        } else {
+            sA[threadIdx.y][threadIdx.x] = 0.0;
+        }
+        if (Col < w_out && (threadIdx.y + ph * piastrella) < w_in1) {
+            sB[threadIdx.y][threadIdx.x] = in2[(threadIdx.y + ph * piastrella) * w_out + Col];
+        } else {
+            sB[threadIdx.y][threadIdx.x] = 0.0;
+        }
+        __syncthreads();
+
+        for (int j = 0; j < piastrella; ++j) {
+            Cvalue += sA[threadIdx.y][j] * sB[j][threadIdx.x];
+        }
+    }
+    if (Row < h_out && Col < w_out) {
+        out[Row * w_out + Col] = Cvalue;
+    }
+}
+
+/*__global__ void matrix_product(float *in1, float *in2, float *out, int w_out, int h_out, int w_in1){
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
     int idy = blockDim.y * blockIdx.y + threadIdx.y;
 
@@ -139,11 +173,42 @@ __global__ void matrix_product(float *in1, float *in2, float *out, int w_out, in
         }
 
         out[idy * w_out + idx] = tmp;
-        //printf("\t(%d, %d)%e\n", idy, idx, out[idy * w_out + idx]);
+    }
+}*/
+
+__global__ void matrix_transpose_product(float *in1, float *in2, float *out, int w_out, int h_out, int h_in1, int piastrella) {
+    __shared__ float sA[TILE_DIM][TILE_DIM];   // Tile size of 32x32
+    __shared__ float sB[TILE_DIM][TILE_DIM];
+
+    int Row = blockDim.y * blockIdx.y + threadIdx.y;
+    int Col = blockDim.x * blockIdx.x + threadIdx.x;
+    float Cvalue = 0.0;
+    sA[threadIdx.y][threadIdx.x] = 0.0;
+    sB[threadIdx.y][threadIdx.x] = 0.0;
+
+    for (int ph = 0; ph < (((h_in1 - 1) / piastrella) + 1); ph++) {
+        if ((Row < h_out) && (threadIdx.x + (ph * piastrella)) < h_in1) {
+            sA[threadIdx.y][threadIdx.x] = in1[(threadIdx.x + ph * piastrella) * h_out + Row]; // (Row * h_in1) + threadIdx.x + (ph * piastrella)
+        } else {
+            sA[threadIdx.y][threadIdx.x] = 0.0;
+        }
+        if (Col < w_out && (threadIdx.y + ph * piastrella) < h_in1) {
+            sB[threadIdx.y][threadIdx.x] = in2[(threadIdx.y + ph * piastrella) * w_out + Col];
+        } else {
+            sB[threadIdx.y][threadIdx.x] = 0.0;
+        }
+        __syncthreads();
+
+        for (int j = 0; j < piastrella; ++j) {
+            Cvalue += sA[j][threadIdx.y] * sB[j][threadIdx.x];
+        }
+    }
+    if (Row < h_out && Col < w_out) {
+        out[Row * w_out + Col] = Cvalue;
     }
 }
 
-__global__ void matrix_transpose_product(float *in1, float *in2, float *out, int w_out, int h_out, int h_in1){
+/*__global__ void matrix_transpose_product(float *in1, float *in2, float *out, int w_out, int h_out, int h_in1){
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
     int idy = blockDim.y * blockIdx.y + threadIdx.y;
 
@@ -155,9 +220,41 @@ __global__ void matrix_transpose_product(float *in1, float *in2, float *out, int
         }
         out[idy * w_out + idx] = tmp;
     }
+}*/
+
+__global__ void matrix_product_transpose(float *in1, float *in2, float *out, int w_out, int h_out, int w_in1, int piastrella) { //w_in1 rappresenta la dimensione comune delle due matrici di ingresso
+    __shared__ float sA[TILE_DIM][TILE_DIM];   // Tile size of 32x32
+    __shared__ float sB[TILE_DIM][TILE_DIM];
+
+    int Row = blockDim.y * blockIdx.y + threadIdx.y;
+    int Col = blockDim.x * blockIdx.x + threadIdx.x;
+    float Cvalue = 0.0;
+    sA[threadIdx.y][threadIdx.x] = 0.0;
+    sB[threadIdx.y][threadIdx.x] = 0.0;
+
+    for (int ph = 0; ph < (((w_in1 - 1) / piastrella) + 1); ph++) {
+        if ((Row < h_out) && (threadIdx.x + (ph * piastrella)) < w_in1) {
+            sA[threadIdx.y][threadIdx.x] = in1[(Row * w_in1) + threadIdx.x + (ph * piastrella)];
+        } else {
+            sA[threadIdx.y][threadIdx.x] = 0.0;
+        }
+        if (Col < w_out && (threadIdx.y + ph * piastrella) < w_in1) {
+            sB[threadIdx.y][threadIdx.x] = in2[(Col * w_in1) + threadIdx.y + (ph * piastrella)]; //[(threadIdx.y + ph * piastrella) * w_out + Col];
+        } else {
+            sB[threadIdx.y][threadIdx.x] = 0.0;
+        }
+        __syncthreads();
+
+        for (int j = 0; j < piastrella; ++j) {
+            Cvalue += sA[threadIdx.y][j] * sB[j][threadIdx.x];//sA[threadIdx.y][j] * sB[j][threadIdx.x];
+        }
+    }
+    if (Row < h_out && Col < w_out) {
+        out[Row * w_out + Col] = Cvalue;
+    }
 }
 
-__global__ void matrix_product_transpose(float *in1, float *in2, float *out, int w_out, int h_out, int w_in1){
+/*__global__ void matrix_product_transpose(float *in1, float *in2, float *out, int w_out, int h_out, int w_in1){
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
     int idy = blockDim.y * blockIdx.y + threadIdx.y;
 
@@ -169,7 +266,7 @@ __global__ void matrix_product_transpose(float *in1, float *in2, float *out, int
         }
         out[idy * w_out + idx] = tmp;
     }
-}
+}*/
 
 __global__ void matrix_dot_product(float *in1, float *in2, float *out, int w, int h){
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
@@ -181,41 +278,36 @@ __global__ void matrix_dot_product(float *in1, float *in2, float *out, int w, in
     }
 }
 
-__global__ void matrix_scalar_product(float *io, float scalar, int w, int h, int stampa){
+__global__ void matrix_scalar_product(float *io, float scalar, int w, int h){
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
     int idy = blockDim.y * blockIdx.y + threadIdx.y;
 
     if(idx < w && idy < h){
-        io[idy * w + idx] = io[idy * w + idx] * scalar;
-        if(stampa == 1) printf("\t(%d, %d) %e\n", idy, idx, io[idy * w + idx]);
+        int index = idy * w + idx;
+        io[index] = io[index] * scalar;
     }
 }
 
-__global__ void matrix3D_scalar_product(float *io, float scalar, int w, int h, int stampa){
+__global__ void matrix3D_scalar_product(float *io, float scalar, int w, int h){
     int c = blockIdx.x;
     int idx = threadIdx.x;
     int idy = threadIdx.y;
 
     if(idx < w && idy < h){
-        io = io + (c * w * h);
-        io[idy * w + idx] = io[idy * w + idx] * scalar;
-        if(stampa == 1) printf("\t(%d, %d) %e\n", idy, idx, io[idy * w + idx]);
+        int index = idy * w + idx + c * w * h;
+        io[index] = io[index] * scalar;
     }
 }
 
-__global__ void tanh(float *in, int w, int h, int stampa){
+__global__ void tanh(float *in, int w, int h){
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
     int idy = blockDim.y * blockIdx.y + threadIdx.y;
 
     if(idx < w && idy < h){
-        float val = in[idy * w + idx];
-        float p = expf(val);
-        float m = expf(-val);
-
-        in[idy * w + idx] = (p - m) / (p + m);
-
-        //in[idy * w + idx] = 1.0 / (1.0 + exp(in[idy * w + idx]));
-        if(stampa == 1) printf("\t(%d, %d) %e\n", idy, idx, in[idy * w + idx]);
+        int index = idy * w + idx;
+        float val = in[index];
+        float e = expf(2 * val);
+        in[index] = (e - 1) / (e + 1);
     }
 }
 
@@ -240,7 +332,6 @@ __global__ void scalar_subtraction(float *out, float *in, int w, int h){
     int idy = blockDim.y * blockIdx.y + threadIdx.y;
 
     if(idx < w && idy < h){
-        
         int index = idy * w + idx;
         out[index] = 1 - in[index];
     }
@@ -248,20 +339,16 @@ __global__ void scalar_subtraction(float *out, float *in, int w, int h){
 
 __global__ void subtraction_scalar_parametric(float *io, float scalar, int w, int h){
     int c = blockIdx.x;
-
     int idx = threadIdx.x;
     int idy = threadIdx.y;
 
-
-
     if(idx < w && idy < h){
-        io = io + (c * w * h);
-        int index = idy * w + idx;
+        int index = idy * w + idx + c * w * h;
         io[index] = io[index] - scalar;
     }
 }
 
-__global__ void transpose(float *out, float *in, int w_out, int h_out){         // BlockDim = alle dimensioni della matrice di uscita
+__global__ void transpose(float *out, float *in, int w_out, int h_out){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int idy = blockIdx.y * blockDim.y + threadIdx.y;
 
